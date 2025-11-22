f39c015832eabd99a2a886a6038e9d8f5e972197
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2025-11-22 18:12:29 +0100

    Vista lista

--- src/app/views/components/new-student-modal-component/new-student-modal-component.ts
+++ src/app/views/components/new-student-modal-component/new-student-modal-component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-new-student-modal-component',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './new-student-modal-component.html',
   styleUrl: './new-student-modal-component.css',
 })
@@ -12,14 +13,34 @@ export class NewStudentModalComponent implements AfterViewInit {
 
   @ViewChild('modal') modal!: TemplateRef<any>;
 
-  constructor(private router: Router, private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private router: Router) {}
+
 
   openModal(modalContent: TemplateRef<any>) {
-    this.modalService.open(modalContent, { centered: true, backdrop: 'static' })
+    this.modalService.open(modalContent, { centered: true, backdrop: 'static', keyboard: false });
   }
 
   ngAfterViewInit(): void {
     this.openModal(this.modal);
+  }
+
+  studentFormGroup: FormGroup = new FormGroup({
+    // Define form controls here as needed
+    nameFormControl: new FormControl('',[Validators.required, Validators.minLength(1)]),
+    lastNameFormControl: new FormControl('',[Validators.required, Validators.minLength(1)]),
+    gradeFormControl: new FormControl('',[Validators.required]),
+    disabilityFormControl: new FormControl('',[Validators.required]),
+    photoFormControl: new FormControl('',[Validators.required]),
+  });
+
+  sendData() {
+    if (this.studentFormGroup.valid) {
+      // Process form data here
+      this.modalService.dismissAll();
+      this.router.navigate(['/students']);
+    } else {
+      this.studentFormGroup.markAllAsTouched();
+    }
   }
 
 }
