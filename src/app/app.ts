d002c3c985773a36265823dde3c591237a16f2b2
Mauro Serrano <mauro.serranoo26@gmail.com>
2025-11-22 16:26:30 +0100

    menu funcional para moverse por las dos opciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./views/components/shared/header-component/header-component";
+import { MenuComponent } from "./views/components/shared/menu-component/menu-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
