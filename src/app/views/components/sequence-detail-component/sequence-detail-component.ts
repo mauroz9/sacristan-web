aa229bc809d13688556f1ece0a4acdccba6dcd86
Mauro Serrano <mauro.serranoo26@gmail.com>
2025-12-02 18:38:14 +0100

    cambio a mostrar en forma de diapositivas

--- src/app/views/components/sequence-detail-component/sequence-detail-component.ts
+++ src/app/views/components/sequence-detail-component/sequence-detail-component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Sequence } from '../../../logic/interfaces/sequence-interface';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SequenceService } from '../../../logic/services/sequence-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sequence-detail-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sequence-detail-component.html',
   styleUrl: './sequence-detail-component.css',
 })
 export class SequenceDetailComponent implements OnInit {
   sequence: Sequence | undefined;
+  currentIndex: number = 0;
 
   constructor(private route: ActivatedRoute, private router: Router, private sequenceService: SequenceService) { }
 
@@ -19,6 +21,18 @@ export class SequenceDetailComponent implements OnInit {
 
     if(id){
       this.sequence = this.sequenceService.getSequenceById(+id);
+    }
+  }
+
+  prevStep() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  nextStep() {
+    if (this.sequence && this.currentIndex < this.sequence.steps.length - 1) {
+      this.currentIndex++;
     }
   }
 
