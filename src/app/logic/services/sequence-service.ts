ca1070d0051beacfbca745dd5dc43d76a7af8260
Mauro Serrano <mauro.serranoo26@gmail.com>
2025-12-20 16:14:06 +0100

    modificación del servicio para obtener los datos

--- src/app/logic/services/sequence-service.ts
+++ src/app/logic/services/sequence-service.ts
@@ -1,64 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Sequence } from '../interfaces/sequence-interface';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SequenceService {
-  
-  sequenceList: Sequence [] = [
-    {
-      kind: "secuencia",
-      id: 1,
-      title: 'Prepara la mochila',
-      description: 'Elementos necesarios para el colegio',
-      categorie: 'Colegio',
-      steps: [
-        { "id": 1, "order": 1, "description": "Abrir mochila", "imageUrl": "https://api.arasaac.org/api/pictograms/38700" },
-        { "id": 2, "order": 2, "description": "Meter libros", "imageUrl": "https://api.arasaac.org/api/pictograms/37189" },
-        { "id": 3, "order": 3, "description": "Meter el bocata", "imageUrl": "https://api.arasaac.org/api/pictograms/36418" },
-        { "id": 4, "order": 4, "description": "Cerrar mochila", "imageUrl": "https://api.arasaac.org/api/pictograms/38695" },
-        { "id": 5, "order": 5, "description": "Ponerse mochila", "imageUrl": "https://api.arasaac.org/api/pictograms/38265" }
-      ]
-    },
-    {
-      kind: "secuencia",
-      id: 2,
-      title: 'Lavarse las manos',
-      description: 'Higiene antes de comer',
-      categorie: 'Higiene',
-      steps: [
-        { id: 6, order: 1, description: 'Mojar manos', imageUrl: 'https://api.arasaac.org/api/pictograms/8975' },
-        { id: 7, order: 2, description: 'Poner jabón', imageUrl: 'https://api.arasaac.org/api/pictograms/35977' },
-        { id: 8, order: 3, description: 'Frotar', imageUrl: 'https://api.arasaac.org/api/pictograms/36860' }
-      ]
-    }
-  ];
 
-  getSequences(): Sequence[]{
-    return this.sequenceList;
+  constructor (private http: HttpClient) {}
+
+  getSequences(): Observable<Sequence[]>{
+    return this.http.get<Sequence[]>('http://localhost:8000/api/secuencias');
   }
 
-  getSequenceById(id: number): Sequence | undefined {
-    return this.sequenceList.find(s => s.id === id);
+  getSequenceById(id: number): Observable<Sequence | undefined> {
+    return this.http.get<Sequence | undefined>(`http://localhost:8000/api/secuencias/${id}`);
   }
 
   addSequence(newSequence: Sequence){
-    newSequence.id = this.sequenceList.length + 1;
-    this.sequenceList.push(newSequence);
+    this.http.post('http://localhost:8000/api/secuencias', newSequence);
   }
 
   modifySequence(modifySequence: Sequence){
-    const index = this.sequenceList.findIndex(s => s.id === modifySequence.id);
-    if (index !== -1) {
-      this.sequenceList[index] = modifySequence;
-    }
+    
+    this.http.put( `http://localhost:8000/api/secuencias/${modifySequence.id}`, modifySequence);
   }
 
   deleteSequence(id: number){
-    const index = this.sequenceList.findIndex(s => s.id === id)
-    if(index !== -1){
-      this.sequenceList.splice(index, 1)
-    }
+    this.http.delete(`http://localhost:8000/api/secuencias/${id}`)
   }
 }
