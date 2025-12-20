6283e35c3a50d30cadaddf1d974fb85fae4db25d
Mauro Serrano <mauro.serranoo26@gmail.com>
2025-12-20 16:13:43 +0100

    ajuste en la interfaz

--- src/app/logic/interfaces/sequence-interface.ts
+++ src/app/logic/interfaces/sequence-interface.ts
@@ -2,16 +2,6 @@ import { Category } from "./category-sequence-interface";
 import { Step } from "./sequence-step-interface";
 
 export interface Sequence {
-  kind: 'secuencia',
-  id: number;
-  categoria_id: number;
-  titulo: string;
-  descripcion: string;
-  created_at: string;
-  updated_at: string;
-}
-
-export interface SequenceWithDetails {
   id: number;
   categoria_id: number;
   titulo: string;
