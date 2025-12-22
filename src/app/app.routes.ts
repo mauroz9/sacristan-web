2740fd858bfa61e5e4fbfd79ae6beec111a408e3
Mauro Serrano <mauro.serranoo26@gmail.com>
2025-12-22 01:27:46 +0100

    modificacion de las rutas para tener la proteccion se seguridad mediante el AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,40 +2,61 @@ import { Routes } from '@angular/router';
 import { ContentListPage } from './views/pages/content-list-page/content-list-page';
 import { SequenceFormComponent } from './views/components/form-component/sequence-form-component/sequence-form-component';
 import { SequenceDetailComponent } from './views/components/sequence-detail-component/sequence-detail-component';
+import { LoginPage } from './views/pages/login-page/login-page';
+import { authGuard } from './logic/guards/auth-guard';
 
 export const routes: Routes = [
+    // --- RUTA PÚBLICA (LOGIN) ---
+    { 
+        path: 'login', 
+        component: LoginPage 
+    },
+
+    // --- RUTAS PROTEGIDAS (Sequences) ---
     {
         path: 'sequences', 
-        component: ContentListPage
-    },
-    {
-        path: 'students/new',
-        component: ContentListPage
-    },
-    {
-        path: 'students/modify/:id',
-        component: ContentListPage
-    },
-    {
-        path: 'students/asign-sequences/:id',
-        component: ContentListPage
-    },
-    {
-        path: 'students',
-        component: ContentListPage
+        component: ContentListPage,
+        canActivate: [authGuard]
     },
     {
         path: 'sequences/new',
-        component: SequenceFormComponent
+        component: SequenceFormComponent,
+        canActivate: [authGuard]
     },
     {
         path: 'sequences/modify/:id',
-        component: SequenceFormComponent
+        component: SequenceFormComponent,
+        canActivate: [authGuard]
     },
     {
         path: 'sequences/view/:id',
-        component: SequenceDetailComponent
+        component: SequenceDetailComponent,
+        canActivate: [authGuard]
     },
+
+    // --- RUTAS PROTEGIDAS (Students) ---
+    {
+        path: 'students',
+        component: ContentListPage,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'students/new',
+        component: ContentListPage, 
+        canActivate: [authGuard]
+    },
+    {
+        path: 'students/modify/:id',
+        component: ContentListPage,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'students/asign-sequences/:id',
+        component: ContentListPage, 
+        canActivate: [authGuard]
+    },
+
+    // --- REDIRECCIONES ---
     {
         path: '', 
         redirectTo: '/sequences', 
