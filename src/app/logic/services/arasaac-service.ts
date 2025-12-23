6201db80927e9999d7f33e89883123605b9e0755
Mauro Serrano <mauro.serranoo26@gmail.com>
2025-12-23 11:20:57 +0100

    servicio base para la obtencion de pictogramas

---
+++ src/app/logic/services/arasaac-service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ArasaacPictogram } from '../interfaces/arasaac-interface';
+import { HttpClient } from '@angular/common/http';
+
+const baseAPIUrl: string = 'https://api.arasaac.org/v1';
+const language: string = 'es';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArasaacService {
+  
+  constructor(private http: HttpClient){}
+
+  getPictograms(): Observable<ArasaacPictogram[]>{
+    return this.http.get<ArasaacPictogram[]>(`${baseAPIUrl}/pictograms/all/${language}`);
+  }
+
+  getPictogramById(id: number): Observable<ArasaacPictogram>{
+    return this.http.get<ArasaacPictogram>(`${baseAPIUrl}/pictograms/${id}`);
+  }
+}
