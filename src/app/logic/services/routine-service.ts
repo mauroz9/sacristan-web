52e9c3178d91db466ca5e1d59554b9819f4bdae2
mauroz9 <mauro.serranoo26@gmail.com>
2026-03-04 10:24:45 +0100

    feat: servicio de rutinas para obtener rutinas y los parametros de filtro

---
+++ src/app/logic/services/routine-service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Routine } from '../interfaces/routine-interface';
+import { Observable } from 'rxjs';
+import { PageResponse } from '../interfaces/utils/page-interface';
+import { API_URL } from './env';
+import { SortParam } from '../interfaces/content-interface';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class RoutineService {
+
+    constructor(private http: HttpClient) { }
+
+    getRoutines(query: string = "", sortBy: string = "title", sortDir: string = "asc"): Observable<PageResponse<Routine>> {
+        return this.http.get<PageResponse<Routine>>(API_URL + `/api/v1/routines?q=${query}&sort=${sortBy},${sortDir}`);
+    }
+
+    getRoutineById(id: number): Observable<Routine> {
+        return this.http.get<Routine>(API_URL + `/api/v1/routines/${id}`);
+    }
+
+    getSortParams(): Observable<SortParam[]> {
+        return this.http.get<SortParam[]>(API_URL + `/api/v1/routines/sort-params`);
+    }
+
+}
