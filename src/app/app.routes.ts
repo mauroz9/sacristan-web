3b5c74b1bf0b526a05e3f3c4a59911efbff62b7a
mauroserrano <mauro.serranoo26@gmail.com>
2026-03-10 20:41:31 +0100

    feat: plantilla estática para mostrar las statics de dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { LoginPage } from './views/pages/login-page/login-page';
 import { authGuard } from './logic/guards/auth-guard';
 import { ContentNotFoundPage } from './views/pages/content-not-found-page/content-not-found-page';
 import { RoutineFormComponent } from './views/components/form-component/routine-form-component/routine-form-component';
+import { StatisticsPage } from './views/pages/statistics-page/statistics-page';
 
 export const routes: Routes = [
     // --- RUTA PÚBLICA (LOGIN) ---
@@ -13,6 +14,13 @@ export const routes: Routes = [
         path: 'login', 
         component: LoginPage, 
         data: { showMenu: false }
+    },
+
+    // --- DASHBOARD ---
+    {
+        path: 'statistics',
+        component: StatisticsPage,
+        canActivate: [authGuard]
     },
 
     // --- RUTAS PROTEGIDAS (Sequences) ---
